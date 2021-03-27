4639d3c9267ef698b2ef444287533048fa219c29
shadmaan <shadmaan.4f92@gmail.com>
2021-03-27 23:52:40 +0530

    login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -52,7 +53,8 @@ import { RegisterComponent } from './register/register.component';
     MatSidenavModule,
     MatBadgeModule,
     MatCheckboxModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
